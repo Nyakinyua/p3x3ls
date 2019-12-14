79af23a416d26f2f2273bb599c7da2b44c55b1f2
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-14 11:31:43 +0300

    create location and category classes and add a save function to each of the classes

--- images/models.py
+++ images/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=45,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save_location(self):
+        return self.save()
+    
+class Category(models.Model):
+    name = models.CharField(max_length=45,blank=True)
+    
+    def save_category(self):
+        return self.save()
