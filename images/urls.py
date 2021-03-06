e21cb92bb85c2ba009958d50087acbfd978a9793
Nyakinyua <wanyakinyua968@gmail.com>
2021-03-06 11:24:12 +0300

    remove logout url

--- images/urls.py
+++ images/urls.py
@@ -9,7 +9,6 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('image/<int:id>',views.image,name='image'),
     path('search/', views.search_results, name="search_results"),
-    url(r'^logout/$',views.logout_user,name="logout_user"),
     path('uploadPhoto/', views.uploadPhoto, name="uploadPhoto"),
     path('profile/',views.profile,name='profile'),
 ]
